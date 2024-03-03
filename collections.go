d94802bb5a9434b1947344412d0580d48c643ece
fareskato <fareskato>
2024-03-03 14:08:04 +0300

    release v1.1.5 collection

---
+++ collections.go
@@ -0,0 +1,31 @@
+package fkshopify
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/fareskato/fkshopify/fkhttp"
+)
+
+type shopifyCollection struct {
+	ShopifyID     int    `json:"id"`
+	Title         string `json:"title"`
+	ProductsCount int    `json:"products_count"`
+}
+
+type shopifyCollectionResponse struct {
+	Collection shopifyCollection `json:"collection"`
+}
+
+func (s Shopify) GetShopifyCollection(id string) (*shopifyCollection, error) {
+	var colRes shopifyCollectionResponse
+	colUrl := fmt.Sprintf("%s/collections/%s.json", s.InitStoreUrl(), id)
+	colres, res, err := fkhttp.HttpGet(colRes, colUrl)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, err
+	}
+	return &colres.Collection, nil
+}
