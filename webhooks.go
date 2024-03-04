181f67a13d96a6e7ecd0a0dd9173490ba306a782
fareskato <fareskato>
2024-03-04 11:17:13 +0300

    v1.2.9 finished web hooks

--- webhooks.go
+++ webhooks.go
@@ -20,7 +20,25 @@ func (s Shopify) verifyWebHook(data []byte, hmacHeader string) bool {
 	}
 	return hmac.Equal(expectedHMAC, decodedHMAC)
 }
-func WebHookCreateEntity[T any](s Shopify, t T, hmacHeader string, reqBody []byte) (*T, error) {
+
+// WebHookHandleActionEntity uses to habdle web hooks for sources like product, order ....etc
+// this function handle create,update and delete web hooks
+// in case of create or update the sended data from shopify conatins resource filels like
+// for example:
+// {
+// "title": "Example T-Shirt",
+// "updated_at": "2021-12-31T19:00:00-05:00",
+// "vendor": "Acme",
+// .... other fileds
+// }
+// in case of delete the sended data contains only the deleted item id like:
+//
+//	{
+//	  "id": 788032119674292900
+//	}
+//
+// for more unfo refer to: https://shopify.dev/docs/api/admin-rest/2024-01/resources/webhook#event-topics
+func WebHookHandleActionEntity[T any](s Shopify, t T, hmacHeader string, reqBody []byte) (*T, error) {
 	if s.storeWebHookKey == "" {
 		return nil, fkutils.ErrWebHookMissed
 	}
